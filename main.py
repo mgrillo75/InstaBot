7d9b1054c5c19586fa39f0bd9549a1360564c0f7
Miguel A. Grillo <83026103+mgrillo75@users.noreply.github.com>
2022-12-30 13:47:14 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+from instabot import Bot
+
+my_bot = Bot()
+
+#my_bot.login(username="mtheaoa", password="chillY20")
+#my_bot.logout(username="mags755", password="chillY20")
+my_bot.logout(username="mtheaoa", password="chillY20")
+my_bot.login(username="mtheaoa", password="chillY20")
+
+#followers_ids = my_bot.get_user_followers("mags755")
+
+#for count, each_follower in enumerate(followers_ids):
+#    username = my_bot.get_username_from_user_id(each_follower)
+#    print(username)
