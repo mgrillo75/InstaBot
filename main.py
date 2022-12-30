2b8885000998e9303dc4271c63756bb814f7e54d
Miguel A. Grillo <83026103+mgrillo75@users.noreply.github.com>
2022-12-30 13:47:37 -0600

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ my_bot = Bot()
 
 #my_bot.login(username="mtheaoa", password="chillY20")
 #my_bot.logout(username="mags755", password="chillY20")
-my_bot.logout(username="mtheaoa", password="chillY20")
-my_bot.login(username="mtheaoa", password="chillY20")
+#my_bot.logout(username="mtheaoa", password="chillY20")
+#my_bot.login(username="mtheaoa", password="chillY20")
 
 #followers_ids = my_bot.get_user_followers("mags755")
 
